5ba382536284cdcd5d3d3db26a9d6b9cf4d7c188
tnah-hcmus <tnah.work@gmail.com>
2021-01-10 22:25:06 +0700

    Fix missing Menu

--- client/components/layout/HeaderAdmin.js
+++ client/components/layout/HeaderAdmin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
-import {AppBar, Toolbar, Typography, IconButton} from "@material-ui/core";
+import {AppBar, Toolbar, Typography, IconButton, Menu} from "@material-ui/core";
 import SearchUser from "../admin/SearchUser";
 
 const drawerWidth = 240;
